3afde8fdfc62cc36a13ab5c67b6cc9bd82328bdf
ainnoot <ai6chr+github@gmail.com>
2024-05-08 15:15:51 +0200

    chore(cicd): Cleaning up script.

--- cicd-scripts/cut-release-branch.sh
+++ cicd-scripts/cut-release-branch.sh
@@ -1,13 +1,8 @@
 #! /usr/bin/bash
 
-BUMP_RULE=$1
-OLD_VERSION=$(poetry version -s)
-NEW_VERSION=$(poetry version $BUMP_RULE --dry-run -s)
+VERSION=$(poetry version -s)
 
-git checkout -b release-v$NEW_VERSION
-poetry version $BUMP_RULE
-git add pyproject.toml
-git commit -m "bump($BUMP_RULE): Bumping to $NEW_VERSION from $OLD_VERSION."
-git commit --allow-empty -m "release: Releasing version v$NEW_VERSION."
-git tag -a v$NEW_VERSION -m "Tag for version $NEW_VERSION"
+git checkout -b release-v$VERSION
+git commit --allow-empty -m "release: Read-only branch for release v$VERSION."
+git tag -a v$VERSION -m "New version: v$VERSION"
 git push --follow-tags
