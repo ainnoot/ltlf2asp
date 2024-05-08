749e040185282c7f33df8af4dc3a3e5e95d9299a
ainnoot <ai6chr+github@gmail.com>
2024-05-08 15:10:43 +0200

    Refine script

--- cicd-scripts/cut-release-branch.sh
+++ cicd-scripts/cut-release-branch.sh
@@ -8,5 +8,6 @@ git checkout -b release-v$NEW_VERSION
 poetry version $BUMP_RULE
 git add pyproject.toml
 git commit -m "bump($BUMP_RULE): Bumping to $NEW_VERSION from $OLD_VERSION."
+git commit --allow-empty -m "release: Releasing version v$NEW_VERSION."
 git tag -a v$NEW_VERSION -m "Tag for version $NEW_VERSION"
 git push --follow-tags
