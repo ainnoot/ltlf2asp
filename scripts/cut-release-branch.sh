4458abb7ed38b43c59847e869881cb5a77db5da9
ainnoot <ai6chr+github@gmail.com>
2024-05-08 14:55:40 +0200

    Cut release script

---
+++ scripts/cut-release-branch.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+BUMP_RULE=$1
+OLD_VERSION=$(poetry version -s)
+NEW_VERSION=$(poetry version $BUMP_RULE --dry-run -s)
+
+git checkout -b release-v$NEW_VERSION
+poetry version $BUMP_RULE
+git add pyproject.toml
+git commit -m "bump($BUMP_RULE): Bumping to $NEW_VERSION from $OLD_VERSION."
+git tag -a v$NEW_VERSION -m "Tag for version $NEW_VERSION"
+git push --follow-tags
