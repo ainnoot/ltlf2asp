9f938a55321ecc2b32d31781c7d27b6e69583dca
ainnoot <ai6chr+github@gmail.com>
2024-05-13 16:51:47 +0200

    feat(cli): Added script to check models against a formula.

---
+++ ltlf2asp/api.py
@@ -0,0 +1,4 @@
+from ltlf2asp.solve.solver_interface import Solver
+from ltlf2asp.solve.decode_model import Model, SolveResult, SolveStatus
+from ltlf2asp.parser import parse_formula
+
---
+++ ltlf2asp/solve/parse_trace.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from ltlf2asp.solve.decode_model import State
+import json
+
+def parse_state(state_dict):
+    return State({x: (y.lower() == "true") for x, y in state_dict.items()})
+
+def parse_trace(file: str):
+    model = json.loads(file)
+    states = model['model']['states']
+    return tuple(parse_state(s) for s in states)
