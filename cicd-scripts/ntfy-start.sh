7bceed5c2460721919bd04d067937247604cf195
ainnoot <ai6chr+github@gmail.com>
2024-05-14 11:15:48 +0200

    Testing scripts!

---
+++ cicd-scripts/ntfy-start.sh
@@ -0,0 +1,9 @@
+REF=$1
+ACTOR=$2
+REPO=$3
+curl \
+  -H "Title: CI/CD on ref $REF" \
+  -H "Priority: default" \
+  -H "X-Tags: o" \
+  -d "A pipeline run for $REF@$REPO, started by $ACTOR on $REPO" \
+  ntfy.sh/this-is-a-test-3456
