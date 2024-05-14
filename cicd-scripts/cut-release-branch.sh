845b449a81ad97ce002ca6232a4346c40de37775
ainnoot <ai6chr+github@gmail.com>
2024-05-14 18:33:51 +0200

    ci: release workflow

--- cicd-scripts/cut-release-branch.sh
+++ cicd-scripts/cut-release-branch.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/bash
 
+git checkout master
 VERSION=$(poetry version -s)
 
 git checkout -b release-v$VERSION
-git commit --allow-empty -m "release: Read-only branch for release v$VERSION."
+git commit --allow-empty -m "bump: This is a read-only branch for release v$VERSION."
 git tag -a v$VERSION -m "New version: v$VERSION"
 git push --follow-tags
